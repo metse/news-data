bc0a5ac5ecdb9ee16f2c076e93d9551b66c7400e
Metse <omphemetse.phiri@takealot.com>
2023-01-31 19:15:30 +0200

    wip

--- news_data/news_data/app.py
+++ news_data/news_data/app.py
@@ -1,9 +1,40 @@
+import requests
+from typing import Dict
 from pyramid.view import view_config
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from news_data.lib import get_url
+
+def get_data():
+    return [
+        {
+            "source": "",
+            "url": get_url(topic="tesla"),
+        },
+        {
+            "source": "",
+            "url": get_url(topic="bitcoin"),
+        },
+        {
+            "source": "",
+            "url": get_url(topic="microsoft"),
+        },
+    ]
+
 
 @view_config(route_name="news", renderer="json")
 def news(request):
+    sources = get_data()
+    futures = []
+    with ThreadPoolExecutor() as executor:
+
+        for source in sources:
+            futures.append(executor.submit(requests.get, url=source["url"]))
+
+        for future in as_completed(futures):
+            print(future.result())
+
     request.response.status = 200
-    return {"success": "true"}
+    return {}
 
 
 def includeme(config):
---
+++ news_data/news_data/lib.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_URL = "https://newsapi.org/v2"
+
+def get_url(topic: str):
+    api_key = os.environ.get("NEWS_API_KEY")
+    if not api_key:
+        return
+
+    return f"{BASE_URL}/everything?q={topic}sortBy=publishedAt&apiKey={api_key}"
